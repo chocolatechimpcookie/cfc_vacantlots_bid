fa6ceb8fa27aa6ab93c546289b13ca4ca55fbe97
davidruffner <davidruffner@gmail.com>
2017-03-20 22:56:09 -0400

    Add map
    It breaks the ui views, but the side bar still works.

--- app_front/js/controllers/MapCtrl.js
+++ app_front/js/controllers/MapCtrl.js
@@ -1,14 +1,26 @@
-angular.module('vacantlotsApp')
-    .controller('MapCtrl', function()
-    {
-        //uiGmapGoogleMapApi.then(function(maps)
-        //{
-        //});
+var app
+app = angular.module('vacantlotsApp');
 
-        this.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };        
-        //console.log(map);
-    
-    
+app.config(function(uiGmapGoogleMapApiProvider) {
+    console.log('In config')
+    uiGmapGoogleMapApiProvider.configure({
+        key: "input key",
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
     });
-    
-    console.log("test");
+})
+
+app.controller('MapCtrl', function($scope, uiGmapGoogleMapApi) {
+    console.log('In controller')
+    // Do stuff with your $scope.
+    // Note: Some of the directives require at least something to be defined originally!
+    // e.g. $scope.markers = []
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+    // uiGmapGoogleMapApi is a promise.
+    // The "then" callback function provides the google.maps object.
+    uiGmapGoogleMapApi.then(function(maps) {
+    });
+
+
+});
