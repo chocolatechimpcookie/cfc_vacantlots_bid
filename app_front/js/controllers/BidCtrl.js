d41c77880d2b5cb7b24669aaf18d400b5c589e5b
davidruffner <davidruffner@gmail.com>
2017-03-29 19:53:48 -0400

    Starting to connect front and back ends.

--- app_front/js/controllers/BidCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('vacantlotsApp')
-    .controller('BidCtrl', function($scope, sharedProperties)
-    {
-        console.log('In bid control.')
-        $scope.propertyName = sharedProperties.getString();
-
-    });
