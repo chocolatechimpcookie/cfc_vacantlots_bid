f8dcb23164f82e77eafb59c733da774ac654b6ae
cina <memaralicina@gmail.com>
2017-03-29 20:48:26 -0400

    Set up for easy merge

--- public/js/controllers/RegisterCtrl.js
+++ public/js/controllers/RegisterCtrl.js
@@ -2,7 +2,10 @@ angular.module('vacantlotsApp')
     .controller('RegisterCtrl', function()
     {
     
-    
+        this.submit = function()
+        {
+            console.log("register clicked");
+        };
     });
     
     console.log("Test Register Ctrl");
