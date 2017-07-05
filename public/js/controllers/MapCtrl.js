c500372c717137a73dac0f2db2e325cc544370e7
davidruffner <davidruffner@gmail.com>
2017-07-04 21:49:49 -0400

    Merge in propertyservercall function.

--- public/js/controllers/MapCtrl.js
+++ public/js/controllers/MapCtrl.js
@@ -31,6 +31,7 @@ angular.module('vacantlotsApp').controller('MapCtrl', ['$state', '$http', 'share
   }
 
   vm.markers= [];
+  vm.locations = [];
 
   vm.panorama = new google.maps.StreetViewPanorama(document.getElementById('streetview'));
   var sv = new google.maps.StreetViewService();
@@ -39,105 +40,121 @@ angular.module('vacantlotsApp').controller('MapCtrl', ['$state', '$http', 'share
 
   document.getElementById('streetview').style.display = 'none';
 
-  $http.get('/map').then(function success(res)
+  var getProperties = sharedpropertiesService.getProperties();
+  if (getProperties.length > 1)
   {
-    var properties = res.data;
-    var address="";
-    var tmpmarkers = [];
-    vm.locations = [];
-    var propertyname = "";
-    var propnamet;
-    console.log(properties[0]);
+    vm.markers = getProperties;
+  }
+  else
+  {
+    propertyservercall();
+  }
 
-    for (var i = 0; i < properties.length; i++)
+  function propertyservercall()
+  {
+    $http.get('/map').then(function success(res)
     {
-      property = properties[i];
-      propnamet= "";
-      propertyname ="";
-      propnamet = property.vitalStreetName.trim();
-      propnamet = propnamet.split(" ");
-      for (var x = 0; x < propnamet.length; x++)
+      console.log("result");
+      console.log(res);
+      var properties = res.data;
+      var address="";
+      var tmpmarkers = [];
+      var propertyname = "";
+      var propnamet;
+      console.log(properties[0]);
+
+      for (var i = 0; i < properties.length; i++)
       {
-        propertyname +=" " + propnamet[x][0] +  propnamet[x].slice(1).toLowerCase();
-      }
-      address =
-      property.vitalHouseNumber
-      + propertyname;
-      ;
+        property = properties[i];
+        propnamet= "";
+        propertyname ="";
+        propnamet = property.vitalStreetName.trim();
+        propnamet = propnamet.split(" ");
+        for (var x = 0; x < propnamet.length; x++)
+        {
+          propertyname +=" " + propnamet[x][0] +  propnamet[x].slice(1).toLowerCase();
+        }
+        address =
+        property.vitalHouseNumber
+        + propertyname;
+        ;
 
+        address =
+        property.vitalHouseNumber
+        + property.vitalStreetName;
+        ;
 
-      address =
-      property.vitalHouseNumber
-      + property.vitalStreetName;
-      ;
-      var propertyLatLng = new google.maps.LatLng(property.latitude,
+        var propertyLatLng = new google.maps.LatLng(property.latitude,
         property.longitude);
-      var propertyMarker = new google.maps.Marker({
-        position: propertyLatLng
-      });
-      tmpmarkers.push(propertyMarker)
+        var propertyMarker = new google.maps.Marker({
+          position: propertyLatLng
+        });
+        tmpmarkers.push(propertyMarker)
 
-      vm.locations.push([address, property.latitude, property.longitude, i])
-
-    }
-    vm.markers = tmpmarkers;
-  }, function err(res)
-  {
-    console.log(res);
-  }).then(function success(res)
-  {
-    //Getting the map is asynchronous. You don't get the map until the callback function is executed.
-    // So we have to wait for the map in order to set the property markers
-    NgMap.getMap()
-    NgMap.getMap().then(function(map)
-    {
-      vm.map = map;
-      vm.center = map.getCenter();
-      sv.getPanorama({location: vm.center, radius: 50}, vm.processSVData);
-
-      for (var i = 0; i < vm.markers.length; i++)
-      {
-        var propertyMarker = vm.markers[i]
-        google.maps.event.addListener(propertyMarker, 'click', (function(propertyMarker, i) {
-          return function() {
-            var markerPosition = propertyMarker.getPosition()
-            sv.getPanorama({location: markerPosition, radius: 50}, vm.processSVData);
-
-            //You simultaneously set the streetView location and also get it's new
-            //location. So you need to add a listener that will execute the
-            //getLocation request after you have set the location. This is also important
-            // for the panoramaDate variable.
-            google.maps.event.addListenerOnce(vm.panorama, 'status_changed', function () {
-              addressAndDate = '<div> Address: '+vm.locations[i][0]+'</div><div>Image date: ' + vm.panoramaDate+'</div>'
-              infowindow.setContent(addressAndDate);
-              infowindow.open(vm.map, propertyMarker);
-              document.getElementById('streetview').style.display = '';
-
-              var heading = google.maps.geometry.spherical.computeHeading(vm.panorama.getLocation().latLng,
-                                                                      markerPosition);
-              vm.panorama.setPov({
-                heading: heading,
-                pitch: 0
-              });
-              vm.panorama.setVisible(true);
-              setTimeout(function() {
-              marker = new google.maps.Marker({
-                position: markerPosition,
-                map: vm.panorama,
-              });
-              if (marker && marker.setMap) marker.setMap(vm.panorama);}, 500);
-            });
-          }
-        })(propertyMarker, i));
+        vm.locations.push([address, property.latitude, property.longitude, i])
       }
+      vm.markers = tmpmarkers;
+      // console.log("these are the markers");
+      console.log(vm.markers);
+      sharedpropertiesService.setProperties(vm.markers);
 
-      var markerCluster = new MarkerClusterer(vm.map,
-                 vm.markers, {imagePath: 'https://googlemaps.github.io/js-marker-clusterer/images/m'});
+        // So here, we either push the markers directly into vm.markers or we
+        // create a temp array and then then make markers equivelent to it
 
+      }, function err(res)
+    {
+      console.log(res);
     });
-  }, function err(res)
+  }
+
+
+  //Getting the map is asynchronous. You don't get the map until the callback function is executed.
+  // So we have to wait for the map in order to set the property markers
+  NgMap.getMap().then(function(map)
   {
-    console.log(res)
-  })
+  vm.map = map;
+  vm.center = map.getCenter();
+  sv.getPanorama({location: vm.center, radius: 50}, vm.processSVData);
+
+  for (var i = 0; i < vm.markers.length; i++)
+  {
+    var propertyMarker = vm.markers[i]
+    google.maps.event.addListener(propertyMarker, 'click', (function(propertyMarker, i) {
+      return function() {
+        var markerPosition = propertyMarker.getPosition()
+        sv.getPanorama({location: markerPosition, radius: 50}, vm.processSVData);
+
+        //You simultaneously set the streetView location and also get it's new
+        //location. So you need to add a listener that will execute the
+        //getLocation request after you have set the location. This is also important
+        // for the panoramaDate variable.
+        google.maps.event.addListenerOnce(vm.panorama, 'status_changed', function () {
+          addressAndDate = '<div> Address: '+vm.locations[i][0]+'</div><div>Image date: ' + vm.panoramaDate+'</div>'
+          infowindow.setContent(addressAndDate);
+          infowindow.open(vm.map, propertyMarker);
+          document.getElementById('streetview').style.display = '';
+
+          var heading = google.maps.geometry.spherical.computeHeading(vm.panorama.getLocation().latLng,
+                                                                  markerPosition);
+          vm.panorama.setPov({
+            heading: heading,
+            pitch: 0
+          });
+          vm.panorama.setVisible(true);
+          setTimeout(function() {
+          marker = new google.maps.Marker({
+            position: markerPosition,
+            map: vm.panorama,
+          });
+          if (marker && marker.setMap) marker.setMap(vm.panorama);}, 500);
+        });
+      }
+    })(propertyMarker, i));
+  }
+
+  var markerCluster = new MarkerClusterer(vm.map,
+             vm.markers, {imagePath: 'https://googlemaps.github.io/js-marker-clusterer/images/m'});
+
+});
 
 }]);
