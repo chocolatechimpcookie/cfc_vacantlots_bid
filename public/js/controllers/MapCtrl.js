5114e10b17ccba979a8a361dc483e19b5f338269
cina <memaralicina@gmail.com>
2017-07-16 16:06:57 -0400

    Formatting the code a bit. Also updated sharedproperties

--- public/js/controllers/MapCtrl.js
+++ public/js/controllers/MapCtrl.js
@@ -1,43 +1,36 @@
-// angular.module('vacantlotsApp').config(function(uiGmapGoogleMapApiProvider)
-// {
-//     console.log('In config');
-//     uiGmapGoogleMapApiProvider.configure(
-//     {
-//         //TODO: Factor this into another js file that isn't tracked with git?
-//         key: "AIzaSyA5sCewJikG42pgRQOIJ_NjnVv3c6O_d6I",
-//         v: '3.20', //defaults to latest 3.X anyhow
-//         libraries: 'weather,geometry,visualization'
-//     });
-// });
-
-
-//this needs to be changed, perhaps in a seperate files with a more descriptive name
-
 /**
  * Processes streetview data from google streetview service. It saves the date of the
  * panorama in the panoramaDate global variable, and it sets the location of the global panorama.
  */
+
+
+
 angular.module('vacantlotsApp').controller('MapCtrl', ['$state', '$http', 'sharedpropertiesService', 'NgMap', function($state, $http, sharedpropertiesService, NgMap)
 {
   var vm = this;
+  vm.markers= [];
+  var center = new google.maps.LatLng(40.7356357, -74.18 );
 
-  vm.processSVData = function processSVData(data, status) {
-    if (status === 'OK') {
+  vm.processSVData = function processSVData(data, status)
+  {
+    if (status === 'OK')
+    {
       vm.panoramaDate = data.imageDate
       vm.panorama.setPano(data.location.pano);
-    } else {
+    }
+    else
+    {
       console.error('Street View data not found for this location.');
     }
   }
 
-  vm.markers= [];
 
-  var center = new google.maps.LatLng(40.7356357, -74.18 );
-  vm.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: center,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
+  vm.map = new google.maps.Map(document.getElementById('map'),
+  {
+    zoom: 13,
+    center: center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
   vm.panorama = new google.maps.StreetViewPanorama(document.getElementById('streetview'));
   var sv = new google.maps.StreetViewService();
@@ -63,29 +56,34 @@ angular.module('vacantlotsApp').controller('MapCtrl', ['$state', '$http', 'share
       propertyname ="";
       propnamet = property.vitalStreetName.trim();
       propnamet = propnamet.split(" ");
+
       for (var x = 0; x < propnamet.length; x++)
       {
         propertyname +=" " + propnamet[x][0] +  propnamet[x].slice(1).toLowerCase();
       }
+
       address =
       property.vitalHouseNumber
       + propertyname;
       ;
 
-
       address =
       property.vitalHouseNumber
       + property.vitalStreetName;
       ;
+
       var propertyLatLng = new google.maps.LatLng(property.latitude,
           property.longitude);
-      var propertyMarker = new google.maps.Marker({
+      var propertyMarker = new google.maps.Marker(
+      {
         position: propertyLatLng
       });
+
       tmpmarkers.push(propertyMarker)
 
       locations.push([address, property.latitude, property.longitude, i])
-      google.maps.event.addListener(propertyMarker, 'click', (function(propertyMarker, i) {
+      google.maps.event.addListener(propertyMarker, 'click', (function(propertyMarker, i)
+      {
         return function() {
           var markerPosition = propertyMarker.getPosition()
           sv.getPanorama({location: markerPosition, radius: 50}, vm.processSVData);
@@ -94,9 +92,11 @@ angular.module('vacantlotsApp').controller('MapCtrl', ['$state', '$http', 'share
           //location. So you need to add a listener that will execute the
           //getLocation request after you have set the location. This is also important
           // for the panoramaDate variable.
-          google.maps.event.addListenerOnce(vm.panorama, 'status_changed', function () {
+          google.maps.event.addListenerOnce(vm.panorama, 'status_changed', function ()
+          {
 
-            addressAndDate = '<div> Address: '+locations[i][0]+'</div><div>Image date: ' + vm.panoramaDate+'</div>'
+            addressAndDate =
+            '<div> Address: '+locations[i][0]+'</div><div>Image date: ' + vm.panoramaDate+'</div>'
             infowindow.setContent(addressAndDate);
             infowindow.open(vm.map, propertyMarker);
             document.getElementById('streetview').style.display = '';
@@ -108,12 +108,18 @@ angular.module('vacantlotsApp').controller('MapCtrl', ['$state', '$http', 'share
               pitch: 0
             });
             vm.panorama.setVisible(true);
-            setTimeout(function() {
-            marker = new google.maps.Marker({
-              position: markerPosition,
-              map: vm.panorama,
-            });
-            if (marker && marker.setMap) marker.setMap(vm.panorama);}, 500);
+
+            setTimeout(function()
+            {
+              marker = new google.maps.Marker(
+              {
+                position: markerPosition,
+                map: vm.panorama,
+              });
+
+              if (marker && marker.setMap) marker.setMap(vm.panorama);
+            }, 500);
+
           });
         }
       })(propertyMarker, i));
