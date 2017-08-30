29560e7c00e2574327d6daae5769e5c20701131c
David Ruffner <davidruffner@ip-172-31-21-171.ec2.internal>
2017-08-30 14:48:53 +0000

    Change port to 8080.

--- server.js
+++ server.js
@@ -446,7 +446,7 @@ app.delete('/favorite/:id', passport.authenticate('jwt', { session: false }), (r
     })
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log('Listening on port ' + port)
